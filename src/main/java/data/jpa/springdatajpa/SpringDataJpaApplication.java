95755b104342a2e9b19eb34473cd71fd7c489457
yunhwan <yhyoon1004@naver.com>
2024-02-12 01:30:03 +0900

    공통 인터페이스 설정

--- src/main/java/data/jpa/springdatajpa/SpringDataJpaApplication.java
+++ src/main/java/data/jpa/springdatajpa/SpringDataJpaApplication.java
@@ -2,8 +2,10 @@ package data.jpa.springdatajpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+//@EnableJpaRepositories(basePackages = "data.jpa.springdatajpa.repository") 부트를 사용안하면 이러한 설정을 해야됨
 public class SpringDataJpaApplication {
 
     public static void main(String[] args) {
--- src/main/java/data/jpa/springdatajpa/repository/TeamRepository.java
+++ src/main/java/data/jpa/springdatajpa/repository/TeamRepository.java
@@ -1,42 +1,7 @@
 package data.jpa.springdatajpa.repository;
 
 import data.jpa.springdatajpa.entity.Team;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.swing.text.html.Option;
-import java.util.List;
-import java.util.Optional;
-
-@Repository
-public class TeamRepository {
-    @PersistenceContext
-    EntityManager em;
-
-    public Team save(Team team) {
-        em.persist(team);
-        return team;
-    }
-
-    public void delete(Team team) {
-        em.remove(team);
-    }
-
-    public List<Team> findAll() {
-        return em.createQuery("select t from Team t", Team.class).getResultList();
-    }
-
-    public Optional<Team> findById(Long id) {
-        Team team = em.find(Team.class, id);
-        return Optional.ofNullable(team);
-    }
-
-    public long count() {
-        return em.createQuery("select count(t) from Team t", Long.class).getSingleResult() ;
-    }
-
-
-    //JPA는 업데이트 메서드가 필요없다, why? JPA는 변경감지 기능이 있어서 영속상태의 엔티티가 변경이 되면 자동으로 업데이트 쿼리를 보냄
-
+public interface TeamRepository extends JpaRepository<Team , Long> {
 }
