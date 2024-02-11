1b1a72d041d32c07c5818b3e1593ec491123f6f3
yunhwan <yhyoon1004@naver.com>
2024-02-11 20:30:33 +0900

    순수 JPA기반 레포지토리 만들기

--- src/main/java/data/jpa/springdatajpa/repository/MemberJPARepository.java
+++ src/main/java/data/jpa/springdatajpa/repository/MemberJPARepository.java
@@ -5,6 +5,9 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public class MemberJPARepository {
 
@@ -16,7 +19,24 @@ public class MemberJPARepository {
         return member;
     }
 
-    public  Member find(Long id) {
+    public void delete(Member member) {
+        em.remove(member);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m ", Member.class).getResultList();
+    }
+
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    public long count() {
+        return em.createQuery("select count(m) from Member m", Long.class).getSingleResult() ;
+    }
+
+    public Member find(Long id) {
         return em.find(Member.class, id);
     }
 }
--- src/test/java/data/jpa/springdatajpa/repository/MemberJPARepositoryTest.java
+++ src/test/java/data/jpa/springdatajpa/repository/MemberJPARepositoryTest.java
@@ -7,6 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
@@ -28,4 +30,37 @@ class MemberJPARepositoryTest {
         assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
         assertThat(findMember).isEqualTo(member);//true -> why? 같은 transactional 안에 있는 엔티티는 서로 같음을 보장한다.
     }
+    
+    @Test
+    public void basicTest () throws Exception{
+
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberJPARepository.save(member1);
+        memberJPARepository.save(member2);
+
+        //단건 조회 검증
+        Member  findMember1 = memberJPARepository.findById(member1.getId()).get();
+        Member findMember2 = memberJPARepository.findById(member2.getId()).get();
+
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+        //리스트 조회 검증
+        List<Member> all = memberJPARepository.findAll();
+        assertThat(all.size()).isEqualTo(2);
+
+        //카운트 검증
+        long count = memberJPARepository.count();
+        assertThat(count).isEqualTo(2);
+
+        memberJPARepository.delete(member1);
+        memberJPARepository.delete(member2);
+
+        long deleteCount  = memberJPARepository.count();
+        assertThat(deleteCount  ).isEqualTo(0 );
+
+
+    }
+    
 }
